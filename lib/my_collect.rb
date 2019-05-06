dac53a051461adebf7580a9c6242ca82cc2f7bda
Maasa Kono <konomaasa@gmail.com>
2019-05-06 01:20:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,9 @@
-
-def my_collect(collection)
+def my_collect(array)
   i = 0
-  new_collection = []
-  while i < collection.length
-    new_collection << yield(collection[i])
-    i += 1
-  end
-  new_collection
+  list = []
+    while i < array.length
+      list << yield(array[i])
+      i += 1
+    end
+  list
 end
